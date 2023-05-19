cb4786712451ee720a16075304a14dad2c6542a0
hoanggiang2912 <109599569+hoanggiang2912@users.noreply.github.com>
2023-05-19 11:04:03 +0700

    day 2 of coding: - done multi tab content - done slider - fix effect - respon section

--- js/main.js
+++ js/main.js
@@ -1,44 +1,67 @@
 
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
+// searchbar
 const searchBar = $('.bar')
 const searchIcon = $('.magni-icon')
-const sliderRightBtn = $('.right-icon')
-const sliderLeftBtn = $('.left-icon')
-const slider = $('.slider')
-const tabItems = $$('.tab__item')
-const tabLine = $('.tab__line')
-const activeTab = $('.tab__item.active')
-const panelItem = $$('.panel__item')
+// tab
+// const tabItems = $$('.tab__item')
+const tabLines = $$('.tab__line')
+const renderActiveTabs = $$('.tab__item.active')
+// const panelItem = $$('.panel__item')
+// show more section
 const moreBtn = $$('.more-btn')
 const moreProductsSection = $$('.more-products')
+// respon nav
+const responOverlay = $('.respon-overlay')
+const responNav = $('.respon-nav')
+const openNavBtn = $('.open-btn')
+const closeNavBtn = $('.close-btn')
+// slider
+const prevBtn = $('.prev-btn')
+const nextBtn = $('.next-btn')
+const sliderItem = $$('.slider__item')
+const sliderMain = $('.slider-main')
 
-tabLine.style.width = `${activeTab.offsetWidth}px`
-tabLine.style.left = `${activeTab.offsetLeft}px`
-tabLine.style.top = `${activeTab.offsetTop + activeTab.offsetHeight}px`
 
+for (let i = 0; i < renderActiveTabs.length ; i++) {
+    tabLines[i].style.width = `${renderActiveTabs[i].offsetWidth}px`
+    tabLines[i].style.left = `${renderActiveTabs[i].offsetLeft}px`
+    tabLines[i].style.top = `${renderActiveTabs[i].offsetTop + renderActiveTabs[i].offsetHeight}px`
+}
 
 const app = {
-    sliderIndex : 1,
     eventHandler () {
         // expand search bar
         searchIcon.addEventListener('click' , () => {
             searchIcon.parentElement.classList.toggle('active')
         })
-        // slider
-
         // product tabs
-        tabItems.forEach((tab , index) => {
-            tab.onclick = () => {
-                $('.tab__item.active').classList.remove('active')
-                $('.panel__item.active').classList.remove('active')
-
-                tab.classList.add('active')
-                panelItem[index].classList.add('active')
-                tabLine.style.width = `${tab.offsetWidth}px`
-                tabLine.style.left = `${tab.offsetLeft}px`
-                tabLine.style.top = `${ tab.offsetTop + tab.offsetHeight}px`
-            }
+        const tabContainers = $$('.tab-container')
+        tabContainers.forEach(item => {
+            item.addEventListener('mouseenter' , () => {
+                item.classList.add('active')       
+                const activeContainer = $('.tab-container.active')
+                if (activeContainer) {
+                    const activeTabItems = $$('.tab-container.active > .tabs > .tab__item')
+                    const activeTabLine = activeContainer.querySelector('.tab__line')
+                    const activePanels = activeContainer.querySelectorAll('.panel__item')
+                    activeTabItems.forEach((tab , index) => {
+                        tab.onclick = () => {
+                            activeContainer.querySelector('.panel__item.active').classList.remove('active')
+                            
+                            tab.classList.add('active')
+                            activePanels[index].classList.add('active')
+                            activeTabLine.style.top = `${tab.offsetTop + tab.offsetHeight}px`
+                            activeTabLine.style.left = `${tab.offsetLeft}px`
+                            activeTabLine.style.width = `${tab.offsetWidth}px`
+                        }
+                    });
+                }         
+            })
+            item.addEventListener('mouseleave' , () => {
+                item.classList.remove('active')
+            })
         });
         // show more products
         moreBtn.forEach((btn , index) => {
@@ -51,17 +74,86 @@ const app = {
                     :btn.innerText = 'Hide away' 
             }
         });
+        // open respon nav
+        openNavBtn.onclick = () => {
+            responNav.style.transform = 'translateX(0)'
+            responOverlay.style.opacity = 1
+            responOverlay.style.visibility = 'visible'
+        }
+        closeNavBtn.onclick = () => {
+            this.closeResponThings()
+        }
+        responOverlay.onclick = () => {
+            this.closeResponThings()
+        }
+        // slider
+        let itemWidth = sliderMain.offsetWidth
+        var firstItem = sliderMain.firstElementChild.cloneNode(true);
+        var lastItem = sliderMain.lastElementChild.cloneNode(true);
+
+        // Append the cloned items to the beginning and end of the carousel
+        sliderMain.appendChild(firstItem);
+        sliderMain.insertBefore(lastItem, sliderMain.firstElementChild);
+
+        // Initialize the current position of the carousel
+        var currentPosition = -itemWidth;
+        sliderMain.style.transform = `translateX(${currentPosition}px)`;
+        nextBtn.onclick = () => {
+            // Move to the next item
+            currentPosition -= itemWidth;
+            // Restrict the position to prevent scrolling beyond the last item
+            currentPosition = Math.max(currentPosition, -itemWidth * (sliderMain.children.length - 1));
+            // Apply the new position to the items container
+            sliderMain.style.transform = `translateX(${currentPosition}px)`;
+            // Check if we've reached the cloned last item
+            if (currentPosition === -(itemWidth * (sliderMain.children.length - 1))) {
+                // Move to the actual first item
+                currentPosition = -itemWidth;
+                // Apply the new position instantly without animation
+                sliderMain.style.transition = 'none';
+                sliderMain.style.transform = `translateX(${currentPosition}px)`;
+
+                // After a short delay, reset the transition to re-enable animation
+                setTimeout(function () {
+                    sliderMain.style.transition = '';
+                }, 10);
+            }
+        }
+        prevBtn.addEventListener('click', () => {
+            // Move to the previous item
+            currentPosition += itemWidth;
+            // Restrict the position to prevent scrolling beyond the first item
+            currentPosition = Math.min(currentPosition, 0);
+            // Apply the new position to the items container
+            sliderMain.style.transform = `translateX(${currentPosition}px)`;
+
+            // Check if we've reached the cloned first item
+            if (currentPosition === 0) {
+                // Move to the actual last item
+                currentPosition = -itemWidth * (sliderMain.children.length - 2);
+                // Apply the new position instantly without animation
+                sliderMain.style.transition = 'none';
+                sliderMain.style.transform = `translateX(${currentPosition}px)`;
+
+                // After a short delay, reset the transition to re-enable animation
+                setTimeout(function () {
+                    sliderMain.style.transition = '';
+                }, 10);
+            }
+        });
     },
-    // show (index) {
-    //     moreProductsSection[index].style.display = 'block'
-    //     moreBtn[index].innerText = 'Hide away'
-    // },
-    // hide (index) {
-    //     moreProductsSection[index].style.display = 'none'
-    //     moreBtn[index].innerText = 'See more'
-    // },
+    closeResponThings () {
+        responNav.style.transform = 'translateX(100%)'
+        this.closeOverlay()
+    },
+    closeOverlay () {
+        responOverlay.style.opacity = 0
+        responOverlay.style.visibility = 'hidden'
+    },
     start () {
         this.eventHandler()
     }
 }
-app.start()
+window.onload = () => {
+    app.start()
+}
